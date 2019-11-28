bf858269ff3b4597db83cf52214701feef86171b
Evan <evanamiesgalonski@gmail.com>
2019-11-27 16:20:32 -0800

    raw data

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -1,3 +1,16 @@
 library(poispkgs)
 
-usethis::use_data("DATASET")
+colours <- 
+      c("black" = "#000000",
+        "red" = "#ff2500",
+        "blue" = "#0433ff",
+        "green4" = "#008b00",
+        "brown" = "#a52a2a",
+        "slategray" = "#707f90",
+        "orchid" = "#da70d6",
+        "lightskyblue" = "#87cefa",
+        "aquamarine4" = "#458b74",
+        "orange3" = "#cd8500",
+        "purple" = "#a034f0")
+
+usethis::use_data(colours)
