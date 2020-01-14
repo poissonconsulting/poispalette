22a4a2821e5013613d23ebe576b19d2629bc9466
Evan <evanamiesgalonski@gmail.com>
2020-01-14 13:31:07 -0800

    more colours and palettes

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -1,25 +1,44 @@
 library(poispkgs)
 
-pois_colours <- 
+pois_colours <-
       c("black" = "#000000",
-        "red" = "#ff2500",
-        "blue" = "#0433ff",
-        "green4" = "#008b00",
-        "brown" = "#a52a2a",
-        "slategray" = "#707f90",
-        "orchid" = "#da70d6",
-        "lightskyblue" = "#87cefa",
-        "aquamarine4" = "#458b74",
-        "orange3" = "#cd8500",
-        "purple" = "#a034f0")
-
+        "legacy_red" = "#ff2500",
+        "legacy_blue" = "#0433ff",
+        "legacy_green" = "#008b00",
+        "legacy_brown" = "#a52a2a",
+        "legacy_gray" = "#707f90",
+        "legacy_orchid" = "#da70d6",
+        "legacy_lightblue" = "#87cefa",
+        "legacy_aquamarine" = "#458b74",
+        "legacy_orange" = "#cd8500",
+        "legacy_purple" = "#a034f0",
+        "cblind_orange" = "#D55E00",
+        "cblind_blue" = "#0072B2",
+        "cblind_green" = "#009E73",
+        "cblind_mango" = "#E69F00",
+        "cblind_yellow" = "#F0E442",
+        "cblind_pink" = "#CC79A7",
+        "cblind_lightblue" = "#56B4E9",
+        "red" = "#E33307",
+        "teal" = "#3088A3",
+        "yellow" = "#FFD200",
+        "purple" = "#6837A2",
+        "blue" ="#254FB9",
+        "green" = "#00B159")
+    
 pois_palettes <- list(
+  legacy1 = c("black", "legacy_red", "legacy_blue", "legacy_green", "legacy_orange",
+              "legacy_gray"),
+  legacy2 = c("black", "legacy_red", "legacy_blue", "legacy_green", "legacy_orange",
+              "legacy_gray", "legacy_purple"),
+  legacy3 = c("black", "legacy_red", "legacy_blue", "legacy_green", "legacy_brown",
+              "legacy_gray", "legacy_orchid", "legacy_lightblue","aquamarine",
+              "legacy_orange"),
+  colour_blind_1 = c("black", "cblind_orange", "cblind_blue", "cblind_green", "cblind_mango",
+                   "cblind_yellow", "cblind_pink", "cblind_lightblue"),
+  new_colours = c("black", "red", "teal", "yellow", "purple", "blue"),
   cool = c("blue", "green"),
-  hot = c("orange3", "red"),
-  p1 = c("black", "red", "blue", "green4", "orange3", "slategray"),
-  p2 = c("black", "red", "blue", "green4", "orange3","slategray", "purple"),
-  p3 = c("black", "red", "blue", "green4", "brown", "slategray", "orchid",
-         "lightskyblue","aquamarine4", "orange3"))
+  hot = c("red", "yellow"))
 
 usethis::use_data(pois_colours, overwrite = TRUE)
 usethis::use_data(pois_palettes, overwrite = TRUE)
