eee9e5199ee452ab8e1cbe802e46cf53c918d760
Evan <evanamiesgalonski@gmail.com>
2020-04-20 10:15:34 -0700

    example data to data-raw

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,6 @@
 library(readr)
-points <- read_rds("~/Poisson/Data/poispallete/example data/points.rds")
-lines <- read_rds("~/Poisson/Data/poispallete/example data/lines.rds")
+points <- read_rds(paste0(getwd(), "/data-raw/points.rds"))
+lines <- read_rds(paste0(getwd(), "/data-raw/lines.rds"))
 
 usethis::use_data(points)
 usethis::use_data(lines)
