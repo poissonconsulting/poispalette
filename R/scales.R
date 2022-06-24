c4bc7b8a6fc04c0603e9ead420e54a098e04a8a4
Evan <evanamiesgalonski@gmail.com>
2022-06-24 10:44:18 -0700

    refactor: reorder funs

--- R/scales.R
+++ R/scales.R
@@ -16,19 +16,6 @@ scale_colour_disc_poisson <- function(palette = getOption("poispalette.colours",
   ggplot2::discrete_scale("colour", paste0("pois_", palette), palette = pal, ...)
 }
 
-#' gradient colour scale constructor for poisson colours
-#'
-#' @param palette Character name of palette in pois_palettes, or selection 
-#' of colour names from pois_cols
-#' @param reverse Boolean indicating whether the palette should be reversed
-#' @param ... Additional arguments passed to scale_color_gradientn()
-#' @export
-scale_colour_grad_poisson <- function(palette = "cool", reverse = FALSE, ...) {
-  
-  pal <- pois_pal_grad(palette = palette, reverse = reverse)
-  ggplot2::scale_color_gradientn(colours = pal, ...)
-}
-
 #' discrete fill scale constructor for poisson colours
 #'
 #' @param palette Character name of palette in pois_palettes
@@ -45,6 +32,19 @@ scale_fill_disc_poisson <- function(palette = "colours", reverse = FALSE, ...) {
   }
   
   ggplot2::discrete_scale("fill", paste0("pois_", palette), palette = pal, ...)
+}
+
+#' gradient colour scale constructor for poisson colours
+#'
+#' @param palette Character name of palette in pois_palettes, or selection 
+#' of colour names from pois_cols
+#' @param reverse Boolean indicating whether the palette should be reversed
+#' @param ... Additional arguments passed to scale_color_gradientn()
+#' @export
+scale_colour_grad_poisson <- function(palette = "cool", reverse = FALSE, ...) {
+  
+  pal <- pois_pal_grad(palette = palette, reverse = reverse)
+  ggplot2::scale_color_gradientn(colours = pal, ...)
 }
 
 #' Gradient fill scale constructor for poisson colours
