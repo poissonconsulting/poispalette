349d1f90216ec65dfc774ca3b30a0b45bbfa07e4
Evan <evanamiesgalonski@gmail.com>
2022-06-24 10:42:38 -0700

    refactor: better function name

--- R/internal.R
+++ R/internal.R
@@ -1,7 +1,7 @@
-subset_palette <- function (pal, ...) {
+make_palette_subsetter <- function (pal, ...) {
   function(n) {
     if(n > length(pal)) err(n, " colours needed, but only ", length(pal), " in chosen colour palette.")
-    as.vector(pal[1:n])
+    pal[1:n] |> as.vector()
   }
 }
 
