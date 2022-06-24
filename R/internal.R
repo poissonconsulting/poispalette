5a82a0578e6bdd57ea16e5a3453d90f1bb638ced
Evan <evanamiesgalonski@gmail.com>
2022-06-24 10:30:51 -0700

    chk_hex function

--- R/internal.R
+++ R/internal.R
@@ -4,3 +4,16 @@ subset_palette <- function (pal, ...) {
     as.vector(pal[1:n])
   }
 }
+
+vld_hex <- function(x) {
+  grepl("^#([A-Fa-f0-9]{6})$", x)
+}
+
+chk_hex <- function(x) {
+  chk_character(x)
+  vld <- vld_hex(x)
+  
+  if(!all(vld)) err("Ivalid hex codes detected: ", paste(x[!vld], collapse = ", "))
+  
+  invisible(x)
+}
