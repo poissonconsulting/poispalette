4b02b6f3aaabd9442beaf8cd5710401ac4bfd53f
Evan <evanamiesgalonski@gmail.com>
2022-06-27 14:01:26 -0700

    accomodate unnamed colours in gradients, fixes #54

--- R/sys.R
+++ R/sys.R
@@ -1,23 +1,25 @@
-.pois_colours <-
-  c(
-    "black" = "#000000",
-    "dark blue" = "#053296",
-    "red" = "#E84D22",
-    "yellow" = "#F7B500",
-    "aqua" = "#00706C",
-    "purple" = "#821C65",
-    "green" = "#63BB42",
-    "blue" = "#90BDE5",
-    "orchid" = "#D888CF",
-    "grey" = "#7D7D7D",
-    "pois_blue1" = "#759dbe",
-    "pois_blue2" = "#abcbe7"
-  )
+.pois_colours <- c(
+  "black" = "#000000",
+  "blue" = "#053296",
+  "red" = "#E84D22",
+  "yellow" = "#F7B500",
+  "aqua" = "#00706C",
+  "purple" = "#821C65",
+  "green" = "#63BB42",
+  "light blue" = "#90BDE5",
+  "orchid" = "#D888CF",
+  "grey" = "#7D7D7D",
+  "pois_blue1" = "#759dbe",
+  "pois_blue2" = "#abcbe7"
+)
+
 
 .pois_palettes <- list(
-  colours = c("black", "dark blue", "red", "yellow", "aqua", "purple", "green", "blue",
-              "orchid", "grey"),
-  cool = c("pois_blue1", "dark blue"),
-  hot = c("yellow", "red")
+  colours = .pois_colours[c("black", "blue", "red", "yellow", "aqua", "purple",
+                             "green", "light blue", "orchid", "grey")],
+  cool = c("#759dbe", "#053296"),
+  hot = c("#F7B500", "#E84D22")
 )
+
+
 
