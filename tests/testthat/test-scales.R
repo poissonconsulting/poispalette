b5c2daf8b8225a65a106f46ff3191d3b35e7c645
Evan <evanamiesgalonski@gmail.com>
2025-01-22 13:27:43 -0800

    remove tests for scale_name

--- tests/testthat/test-scales.R
+++ tests/testthat/test-scales.R
@@ -24,22 +24,3 @@ test_that("scale functions produce objects", {
   scale <- scale_fill_grad_poisson(palette = c("#000000", "#000000"))
   expect_identical(class(scale), c("ScaleContinuous", "Scale", "ggproto", "gg"))
 })
-
-
-test_that("scale naming works", {
-  scale <- scale_colour_disc_poisson("new_name")
-  expect_identical(scale$name, "new_name")
-
-  scale <- scale_fill_disc_poisson("new_name")
-  expect_identical(scale$name, "new_name")
-
-  scale <- scale_colour_grad_poisson("new_name")
-  expect_identical(scale$name, "new_name")
-
-  scale <- scale_fill_grad_poisson("new_name")
-  expect_identical(scale$name, "new_name")
-  
-  scale <- scale_colour_disc_poisson()
-  expect_identical(scale$name, structure(list(), class = "waiver"))
-
-})
